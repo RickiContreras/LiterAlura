b12b77610bc4c1d7729213f4824d8318c4a59e8d
Enrique Contreras <rickiocontreras@gmail.com>
2025-08-02 18:58:08 -0300

    Actualiza LiterAluraApplication

--- src/main/java/com/LiterAlura_G8/LiterAlura/LiterAluraApplication.java
+++ src/main/java/com/LiterAlura_G8/LiterAlura/LiterAluraApplication.java
@@ -1,13 +1,21 @@
 package com.LiterAlura_G8.LiterAlura;
 
+import com.LiterAlura_G8.LiterAlura.principal.Principal;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiterAluraApplication {
+public class LiterAluraApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiterAluraApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal(repository);
+		principal.muestraElMenu();
+
+	}
 }
